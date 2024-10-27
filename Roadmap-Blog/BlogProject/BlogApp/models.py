f5c4a54b4d147d4305b2e778a20b57f61f8ad33d
Akhil-prog-design <akhilrdesai@gmail.com>
2024-10-27 16:24:03 -0400

    Added form and view for CREATE

--- Roadmap-Blog/BlogProject/BlogApp/models.py
+++ Roadmap-Blog/BlogProject/BlogApp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils import timezone
+
 
 class Article(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     content = models.TextField()
-    publish_date = models.DateField(auto_now_add=True)
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.title
--- Roadmap-Blog/BlogProject/BlogApp/views.py
+++ Roadmap-Blog/BlogProject/BlogApp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from .models import Article
+from .forms import ArticleForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def createArticleView(request):
+    form = ArticleForm
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("show_url")
+
+    template_name = "BlogApp/Article_form.html"
+    context = {"form": form}
+    return render(request, template_name, context)
+
+def showArticleView(request):
+    obj = Article.objects.all()
+    template_name = "BlogApp/show.html"
+    context = {"obj": obj}
+    return render(request, template_name, context)
