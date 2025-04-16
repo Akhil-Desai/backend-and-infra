3f71df6cb65d4f5e651424ccf4e9613d5c64f238
Akhil-prog-design <akhilrdesai@gmail.com>
2025-04-15 22:45:08 -0400

    Testing files/config

--- database-migration/core/migration_scanner.py
+++ database-migration/core/migration_scanner.py
@@ -17,7 +17,7 @@ class MigrationScanner:
 
         sorted_files = sorted(migration_files, key=lambda p: int(re.match(r'V(\d+)__', p.name).group(1)))
 
-        for file_path in sorted(Path(self.migration_dir).glob('V*__*.sql')):
+        for file_path in sorted_files:
             #Extract Version and description
 
             match = re.match(r'V(\d+)__(.+)\.sql', file_path.name)
@@ -38,4 +38,5 @@ class MigrationScanner:
                 'checksum': checksum,
             })
 
+        #Migrations are returned in sorted order DO NOT RESORT!
         return migrations
