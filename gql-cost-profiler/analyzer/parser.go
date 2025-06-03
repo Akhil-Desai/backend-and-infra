011c671d3b79bbc9327f3b8a27a360bf4f6b6ae5
Akhil-prog-design <akhilrdesai@gmail.com>
2025-06-02 20:43:08 -0400

    Extracted used nodes

--- gql-cost-profiler/analyzer/parser.go
+++ gql-cost-profiler/analyzer/parser.go
@@ -7,6 +7,12 @@ import (
 	"github.com/vektah/gqlparser/v2/validator"
 )
 
+type Argument struct {
+		Name string
+		Value interface{}
+	}
+
+
 func ParseGQLSchema(schemaStr string) (*ast.Schema, error) {
 	schema,err := gqlparser.LoadSchema(&ast.Source{Name: "schema.graphql", Input: schemaStr})
 	if err != nil {
@@ -27,7 +33,46 @@ func ParseGQLQuery(schema *ast.Schema, queryStr string) (*ast.QueryDocument, err
 	return query, nil
 }
 
-func ExtractUsedVariables(doc *ast.QueryDocument) {
+func ExtractQueryNodes(doc *ast.QueryDocument)(map[string][]Argument) {
 	//For Each SelectionSet we encounter a Field which can contain another Field FragmentSpread and Inline Fragment
 	//We can then recurse on each Field,Fragment,and InlineFragment extracting the Variable and any Arguments used with that Variable
+	used := make(map[string][]Argument)
+	visitedFragments := make(map[string]struct{})
+
+
+	var walkSelections func(selections ast.SelectionSet)
+	walkSelections = func(selections ast.SelectionSet) {
+
+		for _,sel := range selections{
+			switch s := sel.(type){
+			case *ast.Field:
+
+				for _,arg := range s.Arguments {
+					if arg.Value.Kind == ast.Variable {
+						used[s.Name] = append(used[s.Name], Argument{Name: arg.Name, Value: ast.Variable})
+					} else {
+						used[s.Name] = append(used[s.Name], Argument{Name: arg.Name, Value: arg.Value.Raw})
+					}
+				}
+				walkSelections(s.SelectionSet)
+			case *ast.FragmentSpread:
+				if _,exist := visitedFragments[s.Name]; exist {
+					continue
+				}
+				visitedFragments[s.Name] = struct{}{}
+				frag := doc.Fragments.ForName(s.Name)
+				if frag != nil {
+					walkSelections(frag.SelectionSet)
+				}
+			case *ast.InlineFragment:
+				walkSelections(s.SelectionSet)
+			}
+		}
+	}
+
+	for _,operations := range doc.Operations {
+		walkSelections(operations.SelectionSet)
+	}
+
+	return used
 }
