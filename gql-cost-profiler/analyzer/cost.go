203a68528af28053ff10e1a15af176f6225b248f
Akhil-prog-design <akhilrdesai@gmail.com>
2025-06-10 22:01:18 -0400

    Refactored node extraction to be compatiable with config

---
+++ gql-cost-profiler/analyzer/cost.go
@@ -0,0 +1,11 @@
+package analyzer
+
+import (
+	"fmt"
+	"os"
+	"gopkg.in/yaml.v3"
+)
+
+func YAMLConfig(path string) (CostConfig, error){
+	return (nil, error)
+}
--- gql-cost-profiler/analyzer/parser.go
+++ gql-cost-profiler/analyzer/parser.go
@@ -7,10 +7,17 @@ import (
 	"github.com/vektah/gqlparser/v2/validator"
 )
 
+//We want our extracted nodes to be like Parent -> Field Name -> Arguments
+type QLNode struct {
+	f_name string
+	args   []*Argument
+}
+
 type Argument struct {
-		Name string
-		Value interface{}
-	}
+	name string
+	isVar bool
+	val interface{} //nil or int
+}
 
 
 func ParseGQLSchema(schemaStr string) (*ast.Schema, error) {
@@ -33,46 +40,67 @@ func ParseGQLQuery(schema *ast.Schema, queryStr string) (*ast.QueryDocument, err
 	return query, nil
 }
 
-func ExtractQueryNodes(doc *ast.QueryDocument)(map[string][]Argument) {
+func ExtractQueryNodes(doc *ast.QueryDocument, schema *ast.Schema)(map[string][]QLNode) {
 	//For Each SelectionSet we encounter a Field which can contain another Field FragmentSpread and Inline Fragment
 	//We can then recurse on each Field,Fragment,and InlineFragment extracting the Variable and any Arguments used with that Variable
-	used := make(map[string][]Argument)
+	nodes := make(map[string][]*QLNode)
 	visitedFragments := make(map[string]struct{})
 
 
-	var walkSelections func(selections ast.SelectionSet)
-	walkSelections = func(selections ast.SelectionSet) {
+	var walkSelections func(selections ast.SelectionSet, p_type string)
+	walkSelections = func(selections ast.SelectionSet, p_type string) {
 
 		for _,sel := range selections{
 			switch s := sel.(type){
-			case *ast.Field:
 
-				for _,arg := range s.Arguments {
-					if arg.Value.Kind == ast.Variable {
-						used[s.Name] = append(used[s.Name], Argument{Name: arg.Name, Value: ast.Variable})
-					} else {
-						used[s.Name] = append(used[s.Name], Argument{Name: arg.Name, Value: arg.Value.Raw})
-					}
+			case *ast.Field:
+				//Extract the parent type
+				fieldDef := schema.Types[p_type].Fields.ForName(s.Name)
+				if fieldDef == nil {
+					continue
 				}
-				walkSelections(s.SelectionSet)
+				var newArgs []*Argument
+				for _,a := range s.Arguments {
+					newArgs = append(newArgs,
+						&Argument{
+						name: a.Name,
+						isVar: a.Value.Kind == ast.Variable,
+						val : a.Value.Raw,
+					})
+				}
+				nodes[p_type] = append(nodes[p_type], &QLNode{f_name: s.Name, args: newArgs})
+
+				walkSelections(s.SelectionSet, fieldDef.Type.Name())
+
 			case *ast.FragmentSpread:
-				if _,exist := visitedFragments[s.Name]; exist {
+
+				if _, ok := visitedFragments[s.Name]; ok {
 					continue
 				}
 				visitedFragments[s.Name] = struct{}{}
 				frag := doc.Fragments.ForName(s.Name)
 				if frag != nil {
-					walkSelections(frag.SelectionSet)
+					walkSelections(frag.SelectionSet, frag.TypeCondition)
 				}
 			case *ast.InlineFragment:
-				walkSelections(s.SelectionSet)
+				walkSelections(s.SelectionSet, s.TypeCondition )
 			}
 		}
 	}
 
-	for _,operations := range doc.Operations {
-		walkSelections(operations.SelectionSet)
+	for _,op := range doc.Operations {
+		var rootType string
+		switch op.Operation {
+		case ast.Query:
+			rootType = "Query"
+		case ast.Mutation:
+			rootType = "Mutation"
+		case ast.Subscription:
+			rootType = "Subscription"
+
+		walkSelections(op.SelectionSet, rootType)
+		}
 	}
 
-	return used
+	return nodes
 }
